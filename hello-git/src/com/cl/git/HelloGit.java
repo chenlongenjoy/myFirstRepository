c2d1c3793396df8dd1e5196ecaed28a8d8b59717
chenlongenjoy <1319660869@qq.com>
2019-04-20 16:44:00 +0800

    添加了m2方法

--- hello-git/src/com/cl/git/HelloGit.java
+++ hello-git/src/com/cl/git/HelloGit.java
@@ -4,4 +4,8 @@ public class HelloGit {
     public static void m1(){
     	System.out.println("m1方法执行了！！！");
     }
+    
+    public static void m2(){
+    	System.out.println("m2方法执行了");
+    }
 }
